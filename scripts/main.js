91064530c358aabccaa0ba9083fb2554fde1a9e6
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-03-01 00:55:26 +0800

    increase task complete value and last button double alert features done

--- scripts/main.js
+++ scripts/main.js
@@ -7,15 +7,24 @@ for(let i=0; i<completedButton.length; i++)
 {
     let button=completedButton[i];
     button.addEventListener('click',function(event){
+        // 1.Alert Board updated Successfully
     alert("Board updated Successfully");
+    // Handle Last button double alert 
+    if(i===completedButton.length-1)
+    {
+        alert("Congrates !!! You have completed all the current task.")
+    }
+    // 2.Decrease  Task assign value
     let taskAssign=getelementByIdandValue('Task-assign');
     let dueTask=taskAssign-1;
     document.getElementById('Task-assign').innerText=dueTask;
-    // Complte task 
+    // 3. Increase Complite Task value
     let compltedTask=getelementByIdandValue('complete-task');
     let updatedCompletedTask=compltedTask+1;
     document.getElementById('complete-task').innerText=updatedCompletedTask;
-    console.log(updatedCompletedTask);
+    // 4. button disable 
+    button.setAttribute('disabled', ''); 
+    
 
     })
 }
